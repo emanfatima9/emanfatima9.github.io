e562347a43572fbb8f6d1c3b1f3b40d1c05aebd3
Talha Anees <https://github.com/talhaanees/>
2018-05-27 07:51:09 +0300

    First page almost done

---
+++ js/funcs.js
@@ -0,0 +1,15 @@
+// var imgCount = 8;
+// var dir = '../images/fun/';
+// console.log("it came here");
+// var randomCount = Math.round(Math.random() * (imgCount - 1)) + 1;
+// var images = new Array
+// images[1] = "fun1.jpg",
+// images[2] = "fun2.jpg",
+// images[3] = "fun3.jpg",
+// images[4] = "fun4.jpg",
+// images[5] = "fun5.jpg",
+// images[6] = "fun6.jpg",
+// images[7] = "fun7.jpg",
+// images[8] = "fun8.jpg",
+// console.log("url(" + dir + images[randomCount] + ")");
+// document.getElementById("pics").style.backgroundImage = 'url(' + '"' +  dir + images[randomCount] + '")';
